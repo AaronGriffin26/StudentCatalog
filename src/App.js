2d6a558a5ab8d00b9f05852519ad5aa31c7a23bf
Aaron Griffin <Aaron.Griffin@pyramidci.com>
2020-08-27 22:22:55 -0400

    Starting over

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import {API} from 'aws-amplify';
+import {Amplify, API} from 'aws-amplify';
+
+Amplify.configure()
+API.configure()
 
 class App extends Component {
-    state = {students: [], postResponse: "", getResponse: ""}
+    state = {students: [], postResponse: '', getResponse: ''}
 
     async componentDidMount() {
         // API.post('studentapi', '/student').then(response => {
@@ -11,8 +14,14 @@ class App extends Component {
         // }).catch(error => {
         //     console.log('error posting, response: ' + error)
         // })
-        API.get('studentapi', '/student').then(response => {
-            this.setState({students: response.students, getData: response})
+        API.get('studentapi', '/student', {
+            'queryStringParameters': {
+                'firstName': 'Aaron',
+                'lastName': 'Griffin'
+            }
+        }).then(response => {
+            console.log('success, response: ' + response)
+            this.setState({students: response.students, getResponse: response.greetings})
         }).catch(error => {
             console.log('error fetching, response: ' + error)
         })
@@ -24,15 +33,15 @@ class App extends Component {
                 <h3>H3H3</h3>
                 <div>Post: {this.state.postResponse}</div>
                 <div>Get: {this.state.getResponse}</div>
-                {
-                    this.state.students.map((student, i) => (
-                        <div>
-                            <p>First: {student.firstName}</p>
-                            <p>Last: {student.lastName}</p>
-                            <p>SSN: {student.ssn}</p>
-                        </div>
-                    ))
-                }
+                {/*{*/}
+                {/*    this.state.students.map((student, i) => (*/}
+                {/*        <div>*/}
+                {/*            <p>First: {student.firstName}</p>*/}
+                {/*            <p>Last: {student.lastName}</p>*/}
+                {/*            <p>SSN: {student.ssn}</p>*/}
+                {/*        </div>*/}
+                {/*    ))*/}
+                {/*}*/}
             </div>
         )
     }
